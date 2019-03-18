ba5242d18cec88ca8f5733886d0939da919de41f
Peijun Zhu <zpj.ustc@gmail.com>
2019-03-18 17:08:58 -0400

    init

---
+++ test.sh
@@ -0,0 +1,5 @@
+mkdir -p output
+python3 niconico.py samples/csapp.jpg output/csapp.jpg
+python3 niconico.py samples/algo.jpg output/algo.jpg
+python3 niconico.py samples/dinosaur.jpg output/dinosaur.jpg
+python3 niconico.py samples/ostep.jpg output/ostep.jpg
