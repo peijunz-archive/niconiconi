0ca074839736535b21b74b949e3269fedd1e1ecf
Peijun Zhu <zpj.ustc@gmail.com>
2019-05-16 12:59:52 -0700

    using fg/bg now

--- niconico.py
+++ niconico.py
@@ -3,14 +3,15 @@ import cv2
 import argparse
 
 template = {
-    "src": "nico.png",
+    "src_bg": "orig_bg.png",
+    "src_fg": "orig_fg.png",
     "anchor": [268, 25],
     "size": [430, 312]
 }
 
 
 def nico_book(template, bookname, outname):
-    img_tpl_full = cv2.imread(template["src"], cv2.IMREAD_UNCHANGED)
+    img_tpl_full = cv2.imread(template["src_bg"], cv2.IMREAD_UNCHANGED)
     img_tpl = img_tpl_full[:, :, :3]
     alpha = img_tpl_full[:, :, 3:]/255.
 
@@ -19,6 +20,13 @@ def nico_book(template, bookname, outname):
     s = slice(x, x+dx), slice(y, y+dy)
     img_book = cv2.resize(img_book, (dy, dx))
     img_tpl[s] = img_tpl[s]*alpha[s] + img_book*(1-alpha[s])
+
+
+    img_tpl_fg = cv2.imread(template["src_fg"], cv2.IMREAD_UNCHANGED)
+    img_fg = img_tpl_fg[:, :, :3]
+    alpha = img_tpl_fg[:, :, 3:]/255.
+
+    img_tpl = img_tpl*(1-alpha) + img_fg*(alpha)
 
     if not outname:
         outname = "output-" + bookname
--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@ python3 niconico.py samples/csapp.jpg output/csapp.jpg
 python3 niconico.py samples/algo.jpg output/algo.jpg
 python3 niconico.py samples/dinosaur.jpg output/dinosaur.jpg
 python3 niconico.py samples/ostep.jpg output/ostep.jpg
+python3 niconico.py samples/clrs.png output/clrs.jpg
